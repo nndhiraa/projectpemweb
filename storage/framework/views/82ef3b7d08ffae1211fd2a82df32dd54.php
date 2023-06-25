0450a8cdc3b034320d3af4ffcd340a26461a26f9
nndhiraa <93322131+nndhiraa@users.noreply.github.com>
2023-06-25 10:03:42 +0700

    Add files via upload

---
+++ storage/framework/views/82ef3b7d08ffae1211fd2a82df32dd54.php
@@ -0,0 +1,58 @@
+
+  
+<?php $__env->startSection('title', 'Home Product'); ?>
+  
+<?php $__env->startSection('contents'); ?>
+    <div class="d-flex align-items-center justify-content-between">
+        <h1 class="mb-0">List Product</h1>
+        <a href="<?php echo e(route('products.create')); ?>" class="btn btn-primary">Add Product</a>
+    </div>
+    <hr />
+    <?php if(Session::has('success')): ?>
+        <div class="alert alert-success" role="alert">
+            <?php echo e(Session::get('success')); ?>
+
+        </div>
+    <?php endif; ?>
+    <table class="table table-hover">
+        <thead class="table-primary">
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Product Code</th>
+                <th>Description</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>+
+            <?php if($product->count() > 0): ?>
+                <?php $__currentLoopData = $product; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rs): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <tr>
+                        <td class="align-middle"><?php echo e($loop->iteration); ?></td>
+                        <td class="align-middle"><?php echo e($rs->title); ?></td>
+                        <td class="align-middle"><?php echo e($rs->price); ?></td>
+                        <td class="align-middle"><?php echo e($rs->product_code); ?></td>
+                        <td class="align-middle"><?php echo e($rs->description); ?></td>  
+                        <td class="align-middle">
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <a href="<?php echo e(route('products.show', $rs->id)); ?>" type="button" class="btn btn-secondary">Detail</a>
+                                <a href="<?php echo e(route('products.edit', $rs->id)); ?>" type="button" class="btn btn-warning">Edit</a>
+                                <form action="<?php echo e(route('products.destroy', $rs->id)); ?>" method="POST" type="button" class="btn btn-danger p-0" onsubmit="return confirm('Delete?')">
+                                    <?php echo csrf_field(); ?>
+                                    <?php echo method_field('DELETE'); ?>
+                                    <button class="btn btn-danger m-0">Delete</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            <?php else: ?>
+                <tr>
+                    <td class="text-center" colspan="5">Product not found</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel10project\resources\views/products/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f424467eb1b9ac3f3ccae4eb0163c154.php
@@ -0,0 +1,10 @@
+
+  
+<?php $__env->startSection('title', 'Dashboard - Laravel Admin Panel With Login and Registration'); ?>
+  
+<?php $__env->startSection('contents'); ?>
+  <div class="row">
+    Dashboard
+  </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel10project\resources\views/dashboard.blade.php ENDPATH**/ ?>
